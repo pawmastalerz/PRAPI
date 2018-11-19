f6b7c24e0dbe34207b0248be137724f8138f5b83
Pawel <paw.mastalerz@gmail.com>
2018-11-19 17:51:35 +0100

    Car details changed.

--- Models/Car.cs
+++ Models/Car.cs
@@ -10,12 +10,14 @@ namespace PRAPI.Models
         public DateTime NextInsurancePayment { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
-        public string Class { get; set; }
-        public string Description { get; set; }
-        public int Doors { get; set; }
-        public int Price { get; set; }
         public int Year { get; set; }
+        public string Body { get; set; }
+        public int Doors { get; set; }
+        public string Fuel { get; set; }
+        public string Description { get; set; }
         public float LP100Km { get; set; }
+        public string AirConditioned { get; set; }
+        public int Price { get; set; }
         public string PublicId { get; set; }
         public string PhotoUrl { get; set; }
         public DateTime ReservedFrom { get; set; }
