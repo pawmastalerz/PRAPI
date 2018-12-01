6bdaac5710d77f12ee713b4240a083711c987c6d
Pawel <paw.mastalerz@gmail.com>
2018-12-01 12:47:49 +0100

    Registration DTO bugfix.

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -5,5 +5,10 @@ namespace PRAPI.Dtos
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string EMail { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string StreetNumber { get; set; }
+        public string PostalCode { get; set; }
     }
 }
